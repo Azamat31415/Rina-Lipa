f68fb0be0d61d3e09dc26f32cb6b5b6e3571c5bd
azabr <230147@astanait.edu.kz>
2023-12-21 22:38:51 +0600

    First init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int d = x % y;
+        System.out.println(d);
+
+        scanner.close();
+    }
+}
